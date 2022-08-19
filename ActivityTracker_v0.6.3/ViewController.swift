c6774c2ad908bf8aad6d11c0a8b6e6920a0b2fb2
lk23286 <lk23286@gmail.com>
2022-08-19 16:07:31 +0200

    Initial Commit

---
+++ ActivityTracker_v0.6.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ActivityTracker_v0.6.3
+//
+//  Created by Laszlo Kovacs on 2022. 08. 19..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
