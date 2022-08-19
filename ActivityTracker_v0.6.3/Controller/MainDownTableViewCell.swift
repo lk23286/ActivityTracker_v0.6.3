61be7cb01627b5323a9cd092b6ee93ec21311e49
lk23286 <lk23286@gmail.com>
2022-08-19 17:29:49 +0200

    create Main custom cell

---
+++ ActivityTracker_v0.6.3/Controller/MainDownTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainDownTableViewCell.swift
+//  ActivityTracker_v0.6.3
+//
+//  Created by Laszlo Kovacs on 2022. 08. 19..
+//
+
+import UIKit
+
+class MainDownTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
