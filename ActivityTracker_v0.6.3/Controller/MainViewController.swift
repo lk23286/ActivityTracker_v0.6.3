837f7622e7a1f92650bf21f0629ef8fc72aa8ffd
lk23286 <lk23286@gmail.com>
2022-08-21 20:47:08 +0200

    add percent  format and function that calculates it

--- ActivityTracker_v0.6.3/Controller/MainViewController.swift
+++ ActivityTracker_v0.6.3/Controller/MainViewController.swift
@@ -14,7 +14,6 @@ class MainViewController: UIViewController {
     
     var dataStore = DataStore()
   
-    
     override func viewDidLoad() {
         super.viewDidLoad()
       
@@ -24,38 +23,25 @@ class MainViewController: UIViewController {
         mainTableViewDown.dataSource = self
        
         mainTableViewDown.register(UINib(nibName: K.Main.nibName, bundle: nil), forCellReuseIdentifier: K.Main.Identifier.cellDown)
-        
-        // Do any additional setup after loading the view.
     }
-
 
     @IBAction func mainBarAddButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
         
         if dataStore.MainTableUpArray.count < K.maxNumberOfMainActivity {
-            
             print(dataStore.MainTableUpArray.count, K.maxNumberOfMainActivity)
-            
             let alert = UIAlertController(title: "Add New Activity", message: "", preferredStyle: .alert)
-            
             let action = UIAlertAction(title: "Add Activity", style: .default) { [self] (action) in
-                
                 let newActivity = textField.text!
-                
                 dataStore.MainTableUpArray.append(newActivity)
                 mainTableViewUp.reloadData()
-                
                 addActivityToDownTable(activity: newActivity)
-                
-for memeber in dataStore.MainTableUpArray {print(memeber)}
                    }
-            
                 alert.addTextField { (alertTextField) in
                     alertTextField.placeholder = " Create New Activity"
                     textField = alertTextField
                 }
-            
                 alert.addAction(action)
                     present(alert, animated: true, completion: nil)
         }
@@ -64,43 +50,33 @@ for memeber in dataStore.MainTableUpArray {print(memeber)}
     func addActivityToDownTable(activity: String) {
         
         let downTableRecord = DownTableRecord(activity: activity, achived: "1.0", goal: "2.0", precent: "50%")
-
         dataStore.MainTableDownArray.append(downTableRecord)
-        
-        
         if dataStore.MainTableDownArray.count == 4 {
             dataStore.MainTableDownArray.append(DownTableRecord(activity: "Break", achived: "1.0", goal: "2.0", precent: "50%"))
-            
         }
         mainTableViewDown.reloadData()
     }
   
-    
-    
     @IBAction func mainBarRemoveButtonPressed(_ sender: UIBarButtonItem) {
         
+        if dataStore.MainTableUpArray.count == 4 {
+            dataStore.MainTableDownArray.remove(at: 4)
+            mainTableViewDown.reloadData()
+        }
+        
         if dataStore.MainTableUpArray.count > 0 {
-            
             dataStore.MainTableUpArray.removeLast()
             mainTableViewUp.reloadData()
             dataStore.MainTableDownArray.remove(at: dataStore.MainTableUpArray.count)
             mainTableViewDown.reloadData()
         }
-        else if dataStore.MainTableDownArray.count == 1   {
-            dataStore.MainTableDownArray.removeLast()
-            mainTableViewDown.reloadData()
-        }
-        
-        print("count: \(dataStore.MainTableDownArray.count)")
-        
-
-        
+//        else if dataStore.MainTableUpArray.count == 0   {
+//            dataStore.MainTableDownArray.removeAll()
+//            mainTableViewDown.reloadData()
+//        }
     }
-    
 }
     
-
-
 extension MainViewController: UITableViewDataSource,UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -140,18 +116,27 @@ print(dataStore.MainTableUpArray.count)
             }
             mainCellUp.backgroundColor = UIColor.black
             mainCellUp.textLabel?.font = UIFont.boldSystemFont(ofSize: 22.0)
-            
-            
-            
-            
-            
-            
-       
             return mainCellUp
         }
         
         if tableView == mainTableViewDown {
             let mainCellDown = tableView.dequeueReusableCell(withIdentifier: K.Main.Identifier.cellDown, for: indexPath) as! MainDownTableViewCell
+            
+            switch indexPath.row {
+            case 0:
+                mainCellDown.activityLabel.textColor = UIColor.green
+            case 1:
+                mainCellDown.activityLabel.textColor = UIColor.blue
+            case 2:
+                mainCellDown.activityLabel.textColor = UIColor.yellow
+            case 3:
+                mainCellDown.activityLabel.textColor = UIColor.gray
+            case 4:
+                mainCellDown.activityLabel.textColor = UIColor.white
+            default:
+                mainCellDown.activityLabel.textColor = UIColor.blue
+            }
+            mainCellDown.goalLabel.textColor = UIColor.green
             
             mainCellDown.activityLabel.text = dataStore.MainTableDownArray[indexPath.row].activity
             mainCellDown.achivedLabel.text = dataStore.MainTableDownArray[indexPath.row].achived
@@ -172,29 +157,61 @@ print(dataStore.MainTableUpArray.count)
         return true
     }
     
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//
-//        if tableView == mainTableViewUp {
-//
-//            if editingStyle == .delete {
-//
-//                dataStore.MainTableUpArray.remove(at: indexPath.row)
-//                mainTableViewUp.deleteRows(at: [indexPath], with: .fade)
-//
-//                removeLineFromDownTable(at: indexPath.row)
-//
-//            } else if editingStyle == .insert {
-//                // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-//            }
-//
-//                   }
-//    }
     
-//    func removeLineFromDownTable(at index: Int) {
-//        dataStore.MainTableDownArray.remove(at: index)
-//        mainTableViewDown.reloadData()
-//    }
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView == mainTableViewUp {
+            print(indexPath.row)
+            performSegue(withIdentifier: K.Main.Identifier.segue, sender: self)
+        }
+        if tableView == mainTableViewDown {
+            
+            var textField = UITextField()
+            let activity = dataStore.MainTableDownArray[indexPath.row].activity
+            let actualGoal = dataStore.MainTableDownArray[indexPath.row].goal
+            let actualyAchived = dataStore.MainTableDownArray[indexPath.row].achived
+            
+            let alert = UIAlertController(title: "You can modify Goal of", message: activity, preferredStyle: .alert)
+            let action = UIAlertAction(title: "Add Goal", style: .default) { [self] (action) in
+                let newGoal = textField.text!
+                
+                if newGoal != "" {
+                    dataStore.MainTableDownArray[indexPath.row].goal = newGoal
+                    dataStore.MainTableDownArray[indexPath.row].precent =  calculatePrecentFrom(achived: actualyAchived, goal: newGoal)
+                    mainTableViewDown.reloadData()
+                }
+
+            }
+            
+            alert.addTextField { (alertTextField) in
+                alertTextField.placeholder = actualGoal
+                textField = alertTextField
+            }
+            
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        }
+    }
+  
+    func calculatePrecentFrom( achived: String, goal: String) -> String {
+        
+        let achivedNumber = Double(achived) ?? 1.0
+        let goalNumber = Double(goal) ?? 1.0
+        let percent = achivedNumber / goalNumber
+
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .percent
+        
+        let percentString = formatter.string(from: percent as NSNumber) ?? "---"
+       
+       
+        
+print(percentString)
+        
+        return percentString
+        
+    }
     
     
     
--- ActivityTracker_v0.6.3/Modell/DataStore.swift
+++ ActivityTracker_v0.6.3/Modell/DataStore.swift
@@ -10,10 +10,10 @@ import Foundation
 struct DataStore {
     var MainTableUpArray = ["Swift", "Sport", "Meditation", "Sleep"]
     var MainTableDownArray = [
-        DownTableRecord(activity: "Swift", achived: "2.0", goal: "8.0", precent: "20%"),
+        DownTableRecord(activity: "Swift", achived: "2.0", goal: "10", precent: "20%"),
         DownTableRecord(activity: "Sport", achived: "3.0", goal: "1.0", precent: "300%"),
-        DownTableRecord(activity: "Meditation", achived: "0,5", goal: "0,5", precent: "100%"),
-        DownTableRecord(activity: "Sleep", achived: "6.0", goal: "7.0", precent: "80%"),
+        DownTableRecord(activity: "Meditation", achived: "0.5", goal: "0.5", precent: "100%"),
+        DownTableRecord(activity: "Sleep", achived: "7.0", goal: "7.0", precent: "100%"),
         DownTableRecord(activity: "Break", achived: "1.0", goal: "2.0", precent: "50%")
                         ]
 }
@@ -22,9 +22,9 @@ struct DownTableRecord {
     let activity: String
     let achived: String
     let separator = "of"
-    let goal: String
+    var goal: String
     let unit = "hours"
-    let precent : String
+    var precent : String
     
 }
 
